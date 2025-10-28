055d688389a6a5d9257f8e658a2d9ba514dcd2eb
unknown <olfuxa@gmail.com>
2025-10-28 13:19:13 +0100

    Usuniecie plikow

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const style = require('../utils/style');
-
-module.exports = function(req, res, ip, visitCount, ipVisits) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(style + `<h1>Odwiedziny: ${visitCount}</h1>`);
-  res.write(`<p>Twoje IP: ${ip}, odwiedzin: ${ipVisits[ip]}</p>`);
-  res.end();
-};
--- utils/logger.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-function logError(message) {
-  fs.appendFile('errors.log', `${new Date().toISOString()} - ${message}\n`, () => {});
-}
-
-module.exports = { logError };
